be76394adf2cc91f9ce5de1d63413aa5e3d5e785
Nikhilkumar2311 <120698682+Nikhilkumar2311@users.noreply.github.com>
2023-07-26 17:17:39 +0530

    #Coding Club

---
+++ app/src/main/java/com/example/ui/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ui.ui.theme.HomeScreen
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalFoundationApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HomeScreen()
+            }
+        }
+}
+
